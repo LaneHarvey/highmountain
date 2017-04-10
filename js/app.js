dbc02a3c15ffb132980e8aa56dbaedc3229d0460
Scott Harvey <scottharvey@iMac.local>
2017-04-10 16:04:27 -0600

    try this

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 angular.module('highmountain', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
+
+      var baseUrl = /local|127/.test(window.location.hostname) ? '' : '/highmountainwebsite';
+
         $stateProvider
             .state('home',{
                 url:'/',
-                templateUrl: "highmountainwebsite/views/home.html"
+                templateUrl: baseUrl + "/views/home.html"
             })
             .state('about',{
                 url:'/about',
-                templateUrl: "highmountainwebsite/views/about.html"
+                templateUrl: baseUrl + "/views/about.html"
             })
             .state('services',{
                 url:'/services',
-                templateUrl: "highmountainwebsite/views/services.html"
+                templateUrl: baseUrl + "/views/services.html"
             })
             .state('ownership',{
                 url:'/ownership',
-                templateUrl: "highmountainwebsite/views/ownership.html",
+                templateUrl: baseUrl + "/views/ownership.html",
             })
             .state('contact',{
                 url:'/contact',
-                templateUrl: "highmountainwebsite/views/contact.html",
+                templateUrl: baseUrl + "/views/contact.html",
             });
 
         $urlRouterProvider
             .otherwise('/');
     });
+    var event = new Event('lastJsLoaded');
+    document.dispatchEvent(event);
