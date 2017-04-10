4071efb2789944f64952039c06a01bb33f7d6a61
Lane Harvey <mrlaneharvey@gmail.com>
2017-04-10 14:50:45 -0600

    trying to make pages work

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@ angular.module('highmountain', ['ui.router'])
         $stateProvider
             .state('home',{
                 url:'/',
-                templateUrl: "/views/home.html"
+                templateUrl: "../views/home.html"
             })
             .state('about',{
                 url:'/about',
-                templateUrl: "/views/about.html"
+                templateUrl: "../views/about.html"
             })
             .state('services',{
                 url:'/services',
-                templateUrl: "/views/services.html"
+                templateUrl: "../views/services.html"
             })
             .state('ownership',{
                 url:'/ownership',
-                templateUrl: "/views/ownership.html",
+                templateUrl: "../views/ownership.html",
             })
             .state('contact',{
                 url:'/contact',
-                templateUrl: "/views/contact.html",
+                templateUrl: "../views/contact.html",
             });
 
         $urlRouterProvider
