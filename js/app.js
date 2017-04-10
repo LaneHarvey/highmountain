c8f535713f6b864d00e6539579c070fdd20de53f
Lane Harvey <mrlaneharvey@gmail.com>
2017-04-10 17:11:40 -0600

    crossing my fingers

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-angular.module('highmountain', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home',{
-                url:'/',
-                templateUrl: "js/views/home.html"
-            })
-            .state('about',{
-                url:'/about',
-                templateUrl: "js/views/about.html"
-            })
-            .state('services',{
-                url:'/services',
-                templateUrl: "js/views/services.html"
-            })
-            .state('ownership',{
-                url:'/ownership',
-                templateUrl: "js/views/ownership.html",
-            })
-            .state('contact',{
-                url:'/contact',
-                templateUrl: "js/views/contact.html",
-            });
-        // $urlRouterProvider
-        //     .otherwise('/');
-    });
+angular.module('app',['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
 
+//this is what are going to call the home state
+$stateProvider.state('home', {
+  templateUrl: 'js/templates/home.html',
+  url: '/home'
+})
+
+
+
+.state('about' ,{
+  templateUrl: 'js/templates/about.html',
+  url: '/about'
+})
+.state('services' ,{
+  templateUrl: 'js/templates/services.html',
+  url: '/services'
+})
+.state('ownership' ,{
+  templateUrl: 'js/templates/ownership.html',
+  url: '/ownership'
+})
+.state('contact' ,{
+  templateUrl: 'js/templates/contact.html',
+  url: '/contact'
+})
+//things as they are at this given time.
+});
+//lcd soundsystem
 function openNav() {
-    document.getElementById("mySidenav").style.width = "320px";
-    document.getElementById("mySidenav").style.height = "667px";
-};
-
-    /* Set the width of the side navigation to 0 */
-
+document.getElementById("mySidenav").style.width = "250px";
+}
+/* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-};
+document.getElementById("mySidenav").style.width = "0";
+}
