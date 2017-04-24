c3fd9526a915282df2a8bb7fa88fa7356c90052d
Lane Harvey <mrlaneharvey@gmail.com>
2017-04-24 14:38:33 -0600

    testing email php

--- email.php
+++ email.php
@@ -1,24 +1,24 @@
-<?PHP
-
-$email_from = 'a@noreply.com';
-$email_to = 'mrlaneharvey@gmail.com';
-
-$subject = "High Mtn Contact form";
-
-$message = 'Name: ' . $_POST[‘name’] . "\n"
-. 'Email: ' . $_POST[‘email’] . "\n"
-. 'Phone Number: ' . $_POST[‘phone’] . "\n"
-. 'Message: ' . $_POST[‘message’] . "\n"
-
-
-;
-
-$message = wordwrap($message, 120);
-
-$headers = "From: " . $email_from . "\r\n" . "Reply-to: " . $email_from . "";
-
-mail($email_to, $subject, $message, $headers);
-
-header("Location: http://www.inspectingut.com/#/contact");
-
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+// Create the email and send the message
+$to = 'mrlaneharvey@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
